f285de8f12b569ce35055577e897c67f9a33db10
JakeWh <kolarkuba@gmail.com>
2021-12-16 11:26:34 +0100

    funkční menu

--- program/project_smoothie.py
+++ program/project_smoothie.py
@@ -1,38 +1,127 @@
-import os, re
-import inquirer
+import os, re, inquirer, signal
+# os - vyčištění okna
+# re, inquirer - balíček menu
+# signal - umožňuje zavření terminálu
 
-input("Pro ideální zážitek ze hry prosím maximalizujte okno a stiskněte klávesu.")
+# Barvy pro obrázky
+class colors:
+    reset = "\033[0m"
+
+    # Black
+    fgBlack = "\033[30m"
+    fgBrightBlack = "\033[30;1m"
+    bgBlack = "\033[40m"
+    bgBrightBlack = "\033[40;1m"
+
+    # Red
+    fgRed = "\033[31m"
+    fgBrightRed = "\033[31;1m"
+    bgRed = "\033[41m"
+    bgBrightRed = "\033[41;1m"
+
+    # Green
+    fgGreen = "\033[32m"
+    fgBrightGreen = "\033[32;1m"
+    bgGreen = "\033[42m"
+    bgBrightGreen = "\033[42;1m"
+
+    # Yellow
+    fgYellow = "\033[33m"
+    fgBrightYellow = "\033[33;1m"
+    bgYellow = "\033[43m"
+    bgBrightYellow = "\033[43;1m"
+
+    # Blue
+    fgBlue = "\033[34m"
+    fgBrightBlue = "\033[34;1m"
+    bgBlue = "\033[44m"
+    bgBrightBlue = "\033[44;1m"
+
+    # Magenta
+    fgMagenta = "\033[35m"
+    fgBrightMagenta = "\033[35;1m"
+    bgMagenta = "\033[45m"
+    bgBrightMagenta = "\033[45;1m"
+
+    # Cyan
+    fgCyan = "\033[36m"
+    fgBrightCyan = "\033[36;1m"
+    bgCyan = "\033[46m"
+    bgBrightCyan = "\033[46;1m"
+
+    # White
+    fgWhite = "\033[37m"
+    fgBrightWhite = "\033[37;1m"
+    bgWhite = "\033[47m"
+    bgBrightWhite = "\033[47;1m"
+
+
+
+input("""###########################################################################
+#\033[31mPro ideální zážitek ze hry prosím maximalizujte okno a stiskněte klávesu.\033[0m#
+###########################################################################
+""")
 # Logo hry
-logo = open("logo.txt", "r")
+os.system("cls||clear") # Vyčištění obazovky
+logo = open("logo.ans", "r")
 file_contents = logo.read()
 print(file_contents)
 logo.close()
-# Experient s menu
+
+# Začátek menu
 otazka = [
-  inquirer.List("main_menu",
+    inquirer.List("main_menu",
                 message="VYBER SI",
                 choices=["Nová hra", "Zavřít", "Kredit"],
             ),
 ]
 odpoved = inquirer.prompt(otazka)
-# Konec experimentu s menu
-input("Pokračovat >>>")
+
+if odpoved == {'main_menu': 'Kredit'}:  # Volba Kredit v menu
+    os.system("cls||clear") # Vyčištění obazovky
+    ninjas_logo = open("ninjas_logo.ans", "r")
+    file_contents = ninjas_logo.read()
+    print(file_contents)
+    ninjas_logo.close()
+    print("""    Společný projekt dua \033[34;1mHacker Ninjas\033[0m. První pokus o hříčku po třech týdnech učení se programování.
+    ------------------------------------------------------------------------------------------------
+    Jakub Kolář e:\ kolarkuba@gmail.com
+    Martin Holomek e:\ santexD@seznam.cz
+    ---------------2021------------------
+    """)
+    input("Pokračovat do hry >>>")
+elif odpoved == {'main_menu': 'Zavřít'}:  # Zavře okno terminálu
+    os.kill(os.getppid(), signal.SIGHUP)
+# Konec menu
+
 os.system("cls||clear") # Vyčištění obazovky
 print("-----------------MEZITÍM V MEXIKU-----------------")
 input("Pokračovat >>>")
 os.system("cls||clear") # Vyčištění obazovky
+
 # Tv zprávy
-tv = open("tv.txt", "r")
+tv = open("tv.ans", "r")
 file_contents = tv.read()
 print(file_contents)
 tv.close()
 print("KESADILLA TV: MIMOŘÁDNÉ ZPRAVODAJSTVÍ! POSLECHNĚTE SI NOVINKY Z VALE DE LA NACHOS!!")
 input("Pokračovat >>>")
-os.system('cls||clear') # Vyčištění obazovky
 print("""\033[4mRamína Petrželková\033[0m,,...Děkuji za předání slova ze studia Alejandro. Tady Ramína Petrželková z ranního vysílání KesadillaTV!
-Zrovna se nacházíme na jižní straně města del Nachos a to konkrétně v provincii El brote más Brillante, kde je spolu semnou místní hrdina, filantrop a zastánce chudých a muž jenž plní žaludky všech bez různých skrupulí seňor Cucumberto.
+Zrovna se nacházíme na jižní straně města del Nachos a to konkrétně v provincii El brote más Brillante,
+kde je spolu semnou místní hrdina, filantrop a zastánce chudých a muž jenž plní žaludky všech bez různých skrupulí seňor Cucumberto.
 Tímhle Vás chci za celou naši televizi pozdravit a poděkovat, že jste přijal naše pozvání, ale zrovna k věci!
 Chtěla bych Vás požádat, abyste nám takhle do úvodu našeho rozhovoru řekl pár slov.
-Určitě mnoho našich diváků a také posluchačů zajímá právě důvod, který u Vás odstartoval tu myšlenku, která vedla k největšímu masovému celoplošnému hnojení v dějinách celého Mechika?
-A proč zrovna přišel nápad vše vyzkoušet tady u nás ve Vale de la Nachos, když je veřejně známo, že jste do města přijel teprve nedávno a nemáte vůči němu žádné závazky ani jiný osobní vztah\".""")
-input("Pokračovat >>>")
+""")
+# Otázka zda pokračovat v rozhovoru
+dalsi_cast = [
+    inquirer.List("Dál",
+        message="Pokračovat",
+        choices=["Ano", "Skip"],
+    ),
+]
+odpoved = inquirer.prompt(dalsi_cast)
+if odpoved == {"Dál": "Ano"}:
+    print("""Určitě mnoho našich diváků a také posluchačů zajímá právě důvod, který u Vás odstartoval tu myšlenku, která vedla k největšímu masovému celoplošnému
+    hnojení v dějinách celého Mechika? A proč zrovna přišel nápad vše vyzkoušet tady u nás ve Vale de la Nachos, když je veřejně známo, že jste do města přijel 
+    teprve nedávno a nemáte vůči němu žádné závazky ani jiný osobní vztah?
+    """)
